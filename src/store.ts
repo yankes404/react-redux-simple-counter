9caa10a4fd879f2b8db9d6c28ef908b97a83b565
Philip <106172533+yankes404@users.noreply.github.com>
2024-10-08 15:25:31 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer as counterReducer } from './features/counter/counterReducer';
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
         counter: counterReducer
     }
